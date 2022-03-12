6f4d62d88c402e8fa33a24d9f73d89f0766ad9fc
Shreya Shankar <ss.shankar505@gmail.com>
2022-03-12 13:42:50 -0800

    adding new init

--- streams/__init__.py
+++ streams/__init__.py
@@ -1,9 +1,5 @@
-import streams.create_domain_matrices
-from streams.dataset import Dataset
 from streams.streams_dataset import STREAMSDataset
 
 __all__ = [
-    "Dataset",
     "STREAMSDataset",
-    "create_domain_matrices",
 ]
