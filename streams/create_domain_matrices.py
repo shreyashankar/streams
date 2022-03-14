6855711216d7bbc517c25112c2b5591138e8fa66
Shreya Shankar <ss.shankar505@gmail.com>
2022-03-14 16:05:16 -0700

    Beijing Air Quality dataset

--- streams/create_domain_matrices.py
+++ streams/create_domain_matrices.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 from pathlib import Path
 from torchvision import datasets
-from streams.utils import FullDataset, FullData
+from streams.utils import FullDataset, SimpleDataFrame, RollingDataFrame
 from wilds import get_dataset
 
+import io
 import logging
 import numpy as np
 import os
 import pandas as pd
+import requests
 import subprocess
 import torch
 import torchvision.transforms as transforms
@@ -187,8 +189,58 @@ def get_jeopardy(force_download: bool = False):
     value_matrix = pd.get_dummies(df["Value"]).astype(int).values
     category_matrix = pd.get_dummies(df["Category"]).astype(int).values
 
-    dataset = FullData(df["Question"].values, df["Answer"].values)
+    dataset = SimpleDataFrame(df["Question"].values, df["Answer"].values)
     return dataset, [value_matrix]  # TODO add category matrix
+
+
+def get_air_quality(force_download: bool = False):
+    download_path = os.path.join(HOME, DOWNLOAD_PREFIX, "air_quality")
+    folder_path = os.path.join(download_path, "PRSA_Data_20130301-20170228")
+
+    if force_download or not os.path.exists(folder_path):
+        logging.info("Downloading air quality data")
+        res = requests.get(
+            "https://archive.ics.uci.edu/ml/machine-learning-databases/00501/PRSA2017_Data_20130301-20170228.zip",
+            stream=True,
+        )
+        with zipfile.ZipFile(io.BytesIO(res.content)) as zip_ref:
+            zip_ref.extractall(download_path)
+
+    dfs = []
+    for filename in os.listdir(folder_path):
+        dfs.append(pd.read_csv(os.path.join(folder_path, filename)))
+
+    df = pd.concat(dfs).reset_index(drop=True)
+    df["year"] = df["year"].astype(int)
+    df["month"] = df["month"].astype(int)
+    df["day"] = df["day"].astype(int)
+    df["hour"] = df["hour"].astype(int)
+
+    # Make one of the sensors int valued
+    wds = df.wd.unique().tolist()
+    df["wd_raw"] = df["wd"]
+    df["wd"] = df["wd"].apply(lambda x: wds.index(x))
+
+    # Create station matrix
+    station_matrix = pd.get_dummies(df["station"]).astype(int).values
+
+    # Create dataset
+    sensor_cols = [
+        "PM2.5",
+        "PM10",
+        "SO2",
+        "NO2",
+        "CO",
+        "O3",
+        "TEMP",
+        "PRES",
+        "DEWP",
+        "RAIN",
+        "wd",
+        "WSPM",
+    ]
+    dataset = RollingDataFrame(df, sensor_cols, "station")
+    return dataset, [station_matrix]
 
 
 name_to_func = {
@@ -197,4 +249,5 @@ name_to_func = {
     "civilcomments": get_civil_comments,
     "poverty": get_poverty,
     "jeopardy": get_jeopardy,
+    "air_quality": get_air_quality,
 }
